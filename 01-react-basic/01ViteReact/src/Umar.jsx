677e231812cc6b4b90334c73501a4c819fc1bcca
Umar-Haqqi <umer1562001@gmail.com>
2023-10-23 15:26:30 +0500

    core props concepts 07 done

--- 01-react-basic/01ViteReact/src/Umar.jsx
+++ 01-react-basic/01ViteReact/src/Umar.jsx
@@ -3,16 +3,16 @@ function MyName() {    // function should be uppercase with or without vite
 
     return (
         <h2>Username: {userName}</h2>   // anything inside {} will be treated as JS variable
-        )
-    }
-    export default MyName;
+    )
+}
+export default MyName;
 
-// --- recommended 
+// --- recommended
 // file/component name to be uppercase is good practice
 // in vite extension jsx is also a good practice
 // in create-react-app file extension be js or jsx choice
 
 // --- thing about variable in react
 // {} is called expression / evaluated expression
-// {if(true) userName} will not work, 
+// {if(true) userName} will not work,
 // evaluated expression is final outcome
