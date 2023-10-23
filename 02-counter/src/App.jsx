97ba6a08f6f2ae509c8ce6ad6956027bf4877721
Umar-Haqqi <umer1562001@gmail.com>
2023-10-23 16:16:44 +0500

    interview question about useState callBack

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -6,26 +6,45 @@ function App() {
   // let counter = 0; to update UI in react, useState hook is used
 
   // first here is var, second is a function
-  let [counter, setCounter] = useState(0);  // can be let
+  const [counter, setCounter] = useState(0);  // can be let
+
   // const [counter, setCounter] = useState(0);
+  // --this setCounter here is a callback function 
 
   const addvalue = () => {
     // working same as below if its let, right now it is const
     // counter = counter + 1;
     // setCounter(counter);
-    if(counter >= 10) {}
-    else{
-      setCounter(counter + 1); // state and UI sync now
-    }
 
+    // setCounter(counter + 1); // state and UI sync now
+
+    // interview question, will it jump to 5
+    // { batch / it is taken as a repeat, so it will inc by 1.
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // setCounter(counter + 1);
+    // } 
+    // -- useState send update in batches
+
+
+    // setCounter(counter + 1);
+    // prevCounter fetch last updated state 
+    // setCounter((prevCounter) => { 
+    //   return prevCounter + 1 
+    // })
+    
+    setCounter(prevCounter => prevCounter + 1) // shortend
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    // now increament will be by 4
   }
 
   const DelValue = () => {
-    counter = counter - 1;
-    if(counter < 0) {}
-    else{
-      setCounter(counter);
-    }
+    // counter = counter - 1; //if var is let it will work
+
+    setCounter(counter - 1);
   }
 
   return (
@@ -38,7 +57,7 @@ function App() {
         onClick={DelValue}
       >Minus -- </button>
 
-      <p>{counter}</p> 
+      <p>{counter}</p>
       {/* hook will update it everywhere */}
     </>
   )
