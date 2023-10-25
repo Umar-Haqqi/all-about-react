d33f0d96142c6bb78dbe2239b482745ec93037b4
Umar-Haqqi <umer1562001@gmail.com>
2023-10-26 02:23:36 +0500

    Routing setup complete and with a feature of highlighting of current page's link in header

--- 07-react-router/src/components/Header/Header.jsx
+++ 07-react-router/src/components/Header/Header.jsx
@@ -34,6 +34,7 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                    to="/"
                                     // css classes here are with callback function in ``
                                     // for current page indication
                                     className={({ isActive }) =>
@@ -44,6 +45,21 @@ export default function Header() {
                                     }
                                 >
                                     Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+
+                                    // this linking will automatically inject data in isActive 
+                                    // it will only highlight the current page / change text color
+                                    to="/about"
+
+                                    className={({ isActive }) =>
+                                        `${isActive ? "text-orange-700" : "text-gray-700"} 
+                                        block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    About
                                 </NavLink>
                             </li>
                         </ul>
